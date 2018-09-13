6f8388eef1f3e632fced5300259f07c18ca0efe1
mschneiderman <mschneiderman@student.mamkschools.org>
2018-09-13 09:42:01 -0400

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!!");
+        System.out.println("Welcome to APCS");
+        Scanner console = new Scanner(System.in);
+        System.out.print("Enter your name:");
+        String name = console.next();
+        System.out.print("Your name is " + name);
+    }
+}
