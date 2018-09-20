ad83cc008c0a9a7f2252c121fd925fbe8d0d265c
mschneiderman <mschneiderman@student.mamkschools.org>
2018-09-20 09:44:17 -0400

    another line added "hello"

--- src/Chapter7Arrays/WeatherAnalysis.java
+++ src/Chapter7Arrays/WeatherAnalysis.java
@@ -6,29 +6,43 @@ import java.util.Scanner;
 // many days are above the average temperature
 public class WeatherAnalysis {
     public static void main(String[] args) {
-        //Step1: prompt user for number of days
         Scanner console = new Scanner(System.in);
+        int length = days(console);
+        average(console, length);
+    }
+
+    public static int days(Scanner console) {
+        //Step1: prompt user for number of days
         System.out.print("How many days' temperatures? ");
         int length = console.nextInt();
+        return length;
+    }
+
+    public static void average(Scanner console, int length) {
+        //Step2: create for loop that ask user for  high temperature with  a cumulative sum variable
+        //and populate array
         int[] temp = new int[length];
-        int totalTemp = 0;
-        //Step2: create for loop that ask user for  high temperature
-        for(int i = 0; i < length; i++){
+        double totalTemp = 0;
+        for (int i = 0; i < length; i++) {
             System.out.print("Day " + (i + 1) + "'s high temp: ");
             int userTemp = console.nextInt();
             totalTemp += userTemp;
             temp[i] = userTemp;
         }
-        int average = totalTemp / length;
+        averageAbove(length, totalTemp, temp);
+    }
+
+    public static void averageAbove(int length, double totalTemp, int[] temp) {
+        // Step 3: calculate average temp and see how many highs are above that average
+        double average = (Math.round((totalTemp / length) * 10.0) / 10.0);
+        System.out.println("Average temp = " + average);
         int above = 0;
-        for(int i = 0; i < length - 1; i++){
-            if (average < temp[i]){
+        for (int i = 0; i < length; i++) {
+            if (average < temp[i]) {
                 above++;
             }
         }
         System.out.println(above + " days were above average.");
-        // Step3: have a cumulative sum variable running in for loop
-        //also have an array
-        //Step4: Calculate the average temp and print//Step5: calculate how many temps were above the average and print
+
     }
 }
