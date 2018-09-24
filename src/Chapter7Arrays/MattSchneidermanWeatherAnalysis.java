3ac3af047cda36c1c2f8e49528c45cbfb59b56cb
mschneiderman <mschneiderman@student.mamkschools.org>
2018-09-24 09:32:14 -0400

    test

--- src/Chapter7Arrays/MattSchneidermanWeatherAnalysis.java
+++ src/Chapter7Arrays/MattSchneidermanWeatherAnalysis.java
@@ -4,23 +4,21 @@ import java.util.Scanner;
 
 //This program takes user input and calculates the average temperature for n days. Program also identifies how
 // many days are above the average temperature
-public class WeatherAnalysis {
+public class MattSchneidermanWeatherAnalysis {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         int length = days(console);
         average(console, length);
     }
 
-    public static int days(Scanner console) {
-        //Step1: prompt user for number of days
+    public static int days(Scanner console) { //prompts user for number of days and returns that value as length
         System.out.print("How many days' temperatures? ");
         int length = console.nextInt();
         return length;
     }
 
-    public static void average(Scanner console, int length) {
-        //Step2: create for loop that ask user for  high temperature with  a cumulative sum variable
-        //and populate array
+    public static void average(Scanner console, int length){//creates a for loop that asks user for high temp and prints
+        // updates a cumulative sum variable and populates an array
         int[] temp = new int[length];
         double totalTemp = 0;
         for (int i = 0; i < length; i++) {
@@ -32,8 +30,8 @@ public class WeatherAnalysis {
         averageAbove(length, totalTemp, temp);
     }
 
-    public static void averageAbove(int length, double totalTemp, int[] temp) {
-        // Step 3: calculate average temp and see how many highs are above that average
+    public static void averageAbove(int length, double totalTemp, int[] temp) { //calculates average temp and prints &
+        //sees how many highs are above that average and prints that number
         double average = (Math.round((totalTemp / length) * 10.0) / 10.0);
         System.out.println("Average temp = " + average);
         int above = 0;
