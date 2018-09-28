645b9876a10d91316222c5478bcff82450442061
Matt Schneiderman <mschneiderman@student.mamkschools.org>
2018-09-27 22:28:28 -0400

    test

---
+++ src/Chapter7Arrays/Random.java
@@ -0,0 +1,8 @@
+package Chapter7Arrays;
+
+public class Random {
+    public static void main(String[] args){
+        int x = (int)(((Math.random() * (10 - 0))  + 0));
+        System.out.print(x);
+    }
+}
