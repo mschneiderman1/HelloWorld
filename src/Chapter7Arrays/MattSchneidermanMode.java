c417cb0df8fe704706b417d38d194adc57b00595
Matt Schneiderman <mschneiderman@student.mamkschools.org>
2018-10-17 09:56:04 -0400

    test

--- src/Chapter7Arrays/MattSchneidermanMode.java
+++ src/Chapter7Arrays/MattSchneidermanMode.java
@@ -2,26 +2,27 @@ package Chapter7Arrays;
 
 import java.util.Scanner;
 
-public class MattSchneidermanMode {
-    public static void main(String[] args){
+public class MattSchneidermanMode { //This program takes a number and find which digit 0-9 occurs the most(mode)
+    public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        System.out.print("Enter your number: ");
+        System.out.print("Enter your number: "); //prompts the user for the number
         int num = console.nextInt();
-        System.out.println("The mode of " + num +  " is " + mostFrequentDigit(num));
+        System.out.println("The mode of " + num + " is " + mostFrequentDigit(num));
     }
-    public static int mostFrequentDigit(int num){
-        int[] array = new int[10];
-        while(num > 0){
+    public static int mostFrequentDigit (int num){
+        int[] array = new int[10]; //creates array so that we can keep track of occurences.
+        while (num > 0) {
             int digit = num % 10;
             array[digit]++;
             num = num / 10;
         }
         int highest = 0;
-        for(int i = 1; i < array.length - 1; i++){
-            if (array[highest] < array[i]){
+        for (int i = 1; i < array.length - 1; i++) {
+            if (array[highest] < array[i]) {
                 highest = i;
-                }
             }
+        }
         return highest;
     }
 }
+
