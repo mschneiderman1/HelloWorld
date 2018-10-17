81a995ee0f6491a59351d19e77774f5899e6176d
mschneiderman <mschneiderman@student.mamkschools.org>
2018-10-17 09:28:51 -0400

    test

---
+++ src/Chapter7Arrays/MattSchneidermanMode.java
@@ -0,0 +1,16 @@
+package Chapter7Arrays;
+
+import java.util.Scanner;
+
+public class MattSchneidermanMode {
+    public static void main(String[] args){
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int num = console.nextInt();
+        mostFrequentDigit(num);
+    }
+    public static void mostFrequentDigit(int num){
+        int[] array = new int[10];
+
+    }
+}
