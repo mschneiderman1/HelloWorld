f9947e26f541080bf64103cc027444347dfced28
Matt Schneiderman <mschneiderman@student.mamkschools.org>
2018-10-17 09:38:29 -0400

    test

--- src/Chapter7Arrays/MattSchneidermanMode.java
+++ src/Chapter7Arrays/MattSchneidermanMode.java
@@ -5,12 +5,23 @@ import java.util.Scanner;
 public class MattSchneidermanMode {
     public static void main(String[] args){
         Scanner console = new Scanner(System.in);
-        System.out.println("Enter your number: ");
+        System.out.print("Enter your number: ");
         int num = console.nextInt();
-        mostFrequentDigit(num);
+        System.out.println("The mode of " + num +  " is " + mostFrequentDigit(num));
     }
-    public static void mostFrequentDigit(int num){
+    public static int mostFrequentDigit(int num){
         int[] array = new int[10];
-
+        while(num > 0){
+            int digit = num % 10;
+            array[digit]++;
+            num = num / 10;
+        }
+        int highest = 0;
+        for(int i = 1; i < array.length - 1; i++){
+            if (array[highest] < array[i]){
+                highest = i;
+                }
+            }
+        return highest;
     }
 }
