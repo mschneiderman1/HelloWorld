231066dd10beaea516b1adea4586c316a1f6e303
Matt Schneiderman <mschneiderman@student.mamkschools.org>
2018-10-17 11:06:01 -0400

    test

--- src/Chapter7Arrays/MattSchneidermanMode.java
+++ src/Chapter7Arrays/MattSchneidermanMode.java
@@ -10,19 +10,20 @@ public class MattSchneidermanMode { //This program takes a number and find which
         System.out.println("The mode of " + num + " is " + mostFrequentDigit(num));
     }
     public static int mostFrequentDigit (int num){
-        int[] array = new int[10]; //creates array so that we can keep track of occurences.
+        int[] array = new int[10]; //creates array so that we can keep track of occurrences.
         while (num > 0) {
-            int digit = num % 10;
+            int digit = num % 10; //this gets the last digit of the number
             array[digit]++;
-            num = num / 10;
+            num = num / 10; // this gets rid of the last digit
         }
         int highest = 0;
-        for (int i = 1; i < array.length - 1; i++) {
+        for (int i = 1; i < array.length - 1; i++) {// this traverses the array to find which number occurred
+            // the most
             if (array[highest] < array[i]) {
                 highest = i;
             }
         }
-        return highest;
+        return highest; // this returns the mode so it can be printed
     }
 }
 
