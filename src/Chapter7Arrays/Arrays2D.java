952284a8cd2c51195db196191a54b43c3e2b624e
Matt Schneiderman <mschneiderman@student.mamkschools.org>
2018-10-25 09:56:50 -0400

    test

---
+++ src/Chapter7Arrays/Arrays2D.java
@@ -0,0 +1,21 @@
+package Chapter7Arrays;
+import java.util.Arrays;
+
+public class Arrays2D {
+    public static void main(String[] args){
+        int[][] mult = new int[5][10];
+        for(int i = 0; i < mult.length; i++){
+            for(int x = 0; x< mult[i].length; x++){
+                    mult[i][x] = (i + 1) * (x + 1);
+            }
+        }
+        for(int row = 0; row < mult.length; row++){
+            for(int column = 0; column < mult[row].length; column++){
+                System.out.print(mult[row][column] + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
---
+++ src/Chapter7Arrays/Names.java
@@ -0,0 +1,22 @@
+package Chapter7Arrays;
+
+import java.util.Arrays;
+
+public class Names {
+    public static void main(String[] args){
+        String[] names = {"Hello", "Hi", "How are you", "LOL"};
+        System.out.println(Arrays.toString(namesBack(names)));
+
+    }
+    public static String[] namesBack(String[] names) {
+        int back = names.length - 1;
+        for (int i = 0; i < names.length / 2; i++) {
+            String temp = names[i];
+            names[i] = names[back];
+            names[back] = temp;
+            back--;
+        }
+        return names;
+    }
+}
+
