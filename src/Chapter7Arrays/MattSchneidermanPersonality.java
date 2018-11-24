a72c95c91bdcc5544911cb8adb6946b8844827e3
Matt Schneiderman <mschneiderman@student.mamkschools.org>
2018-11-24 16:24:22 -0500

    test

--- src/Chapter7Arrays/MattSchneidermanPersonality.java
+++ src/Chapter7Arrays/MattSchneidermanPersonality.java
@@ -1,20 +1,76 @@
 package Chapter7Arrays;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class MattSchneidermanPersonality{
-    public static void main(String[] args) throws FileNotFoundException{
-        inputFile();
-    }
-
-    public static void inputFile(){
+public class MattSchneidermanPersonality {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner console = new Scanner(System.in);
-        System.out.print("What is the file you want to input?: ");
-        String userFile = console.next();
-        Scanner file = new Scanner(userFile);
+        Scanner input = new Scanner(new File(inputFile(console)));
+        PrintStream output = new PrintStream(new File(outputFile(console)));
+        answerArray(input, output);
 
     }
+
+    public static String inputFile(Scanner console) throws FileNotFoundException {
+        System.out.print("Input file name: ");
+        String userInput = console.next();
+        int x = 0;
+        while (x == 0) {
+            try {
+                Scanner test = new Scanner(new File(userInput));
+                x = 1;
+            } catch (FileNotFoundException e) {
+                x = 0;
+                System.out.print("File not found. Try again: ");
+                userInput = console.next();
+            }
+        }
+        return userInput;
+    }
+
+    public static String outputFile(Scanner console) {
+        System.out.print("Output file name: ");
+        String userOutput = console.next();
+        return userOutput;
+    }
+
+    public static void answerArray(Scanner input, PrintStream output){
+        while(input.hasNextLine()){
+            output.println(input.nextLine() + ":");
+            String answers = input.nextLine();
+            char[] userAnswers = new char[70];
+            for(int i = 0; i < userAnswers.length; i++){
+                userAnswers[i] = answers.charAt(i);
+            }
+            int[] aCount = new int[4];
+            int[] bCount = new int[4];
+            count(aCount, bCount, userAnswers,output);
+        }
+    }
+
+    public static void count(int[] aCount, int[] bCount, char[] userAnswers, PrintStream output){
+       for(int i = 0; i < userAnswers.length; i++){
+           char test = userAnswers[i];
+           test = Character.toUpperCase(test);
+           if (test == 'A'){
+               aCount[0]++;
+           }
+           else if(test == 'B'){
+               bCount[0]++;
+           }
+           i += 6;
+       }
+       output.println(Arrays.toString(aCount));
+       output.println(Arrays.toString(bCount));
+
+    }
+
+
+
 
     // 1) Ask user for input file, check to make sure the file exists.
     // Create scanner to process the file. Also create Print stream that way you can print to the output
