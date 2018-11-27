ce33e105fd0d8970155d5380400b316ffbbbda08
mschneiderman <mschneiderman@student.mamkschools.org>
2018-11-27 09:56:05 -0500

    test

---
+++ src/Ch8Classes/PointV1.java
@@ -0,0 +1,16 @@
+package Ch8Classes;
+
+public class PointV1 {
+    //These are the state fields (varaibles)
+    public int x;
+    public int y;
+
+    //behaviors below (methods)
+    public void translate(int x, int y){
+
+    }
+    public void setLocation(int x, int y){
+
+    }
+
+}
--- src/Chapter7Arrays/MattSchneidermanMode.java
+++ src/Chapter7Arrays/MattSchneidermanMode.java
@@ -6,25 +6,27 @@ import java.util.Scanner;
 public class MattSchneidermanMode {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        System.out.print("Enter your number: "); //prompts user for number
+        System.out.println("Enter your number: ");
+        System.out.print("Enter your number: ");
         int num = console.nextInt();
+        mostFrequentDigit(num);
         System.out.println("The mode of " + num + " is " + mostFrequentDigit(num));
     }
 
-    public static int mostFrequentDigit(int num){
-        int[] array = new int[10]; // This creates an array for the digits 0-9
+    public static int mostFrequentDigit(int num) {
+        int[] array = new int[10];
 
         while (num > 0) {
-            int digit = num % 10; //gets the last digit
-            array[digit]++; // adds one to that index
-            num = num / 10; //gets rid of that digit
+            int digit = num % 10;
+            array[digit]++;
+            num = num / 10;
         }
         int highest = 0;
-        for (int i = 1; i < array.length - 1; i++){ //runs through array to see which index is the highest
+        for (int i = 1; i < array.length - 1; i++) {
             if (array[highest] < array[i]) {
                 highest = i;
             }
         }
-        return highest; //returns the mode
+        return highest;
     }
 }
