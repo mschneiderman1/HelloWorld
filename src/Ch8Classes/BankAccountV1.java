791c69906d524c42d7a115702efc4bdec6a62a90
Matt Schneiderman <mschneiderman@student.mamkschools.org>
2018-12-11 09:57:06 -0500

    add

--- src/Ch8Classes/BankAccountV1.java
+++ src/Ch8Classes/BankAccountV1.java
@@ -3,8 +3,60 @@ package Ch8Classes;
 
 public class BankAccountV1 {
     //State fields below
+    private double balance;
+    private int pin;
+    private String name;
+    private String ssn;
+    private String Address;
+    private String DOB;
 
-    //behaviors below
+    //constructers below
+    public BankAccountV1(String name, String ssn, String Address, String DOB){
+        this.name = name;
+        this.ssn = ssn;
+        this.Address = Address;
+        this.DOB = DOB;
+    }
+    public BankAccountV1(String name, String ssn, String Address, String DOB, int pin, int balance;){
+        this.name = name;
+        this.ssn = ssn;
+        this.Address = Address;
+        this.DOB = DOB;
+        this.pin = pin;
+        this.balance = balance;
+    }
+    //getters
+    public String getName(){
+        return name;
+    }
+    public String getSsn(){
+        return ssn;
+    }
+    public String getAddress(){
+        return Address;
+    }
+    public String getDOB(){
+        return DOB;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public int getPin(){
+        return pin;
+    }
+
+    //setter
+    public void Deposit(double cash){
+        this.balance += cash;
+    }
+    public void Withdrawl(double amount){
+        this.balance -= balance;
+    }
 
     //toString
+
+    public String toString(){
+        return "A";
+    }
+
 }
