e974aa6d2250f1134815652da2bd3472bc575765
Matt Schneiderman <mschneiderman@student.mamkschools.org>
2018-12-17 09:37:14 -0500

    add

--- src/Ch8Classes/BankAccountClientV1.java
+++ src/Ch8Classes/BankAccountClientV1.java
@@ -2,7 +2,7 @@ package Ch8Classes;
 
 public class BankAccountClientV1{
     public static void main(String[] args) {
-        BankAccountV1 Matt = new BankAccountV1("Matt Schneiderman", "123456789",
+        MattSchneidermanBankAccountV1 Matt = new MattSchneidermanBankAccountV1("Matt Schneiderman", "123456789",
                 "940 Fairway Lane", "11012001");
         Matt.Deposit(1000000);
         System.out.println(Matt);
