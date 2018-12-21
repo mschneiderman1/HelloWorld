ec0485a18fc793fd57aa4685dab37118b9b009c0
Matt Schneiderman <mschneiderman@student.mamkschools.org>
2018-12-21 09:51:40 -0500

    add

---
+++ src/Ch8Classes/GroceryProject/MattSchneidermanGroceryItemOrder.java
@@ -0,0 +1,31 @@
+//This program simulates a grocery item
+//This will be a part of a grocery program
+//This will include name, quantity and PricePerUnit
+package Ch8Classes.GroceryProject;
+
+public class MattSchneidermanGroceryItemOrder{
+    //state fields
+    private String name;
+    private int quantity;
+    private double ppu;
+
+    //constructor
+
+    public MattSchneidermanGroceryItemOrder(String name, int quantity, double ppu) {
+        this.name = name;
+        this.quantity = quantity;
+        this.ppu = ppu;
+    }
+    //behaviors
+    public double getCost(){
+        return (ppu * quantity);
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+    public String toString(){
+        return "Name: " + name +"\tQuantity: " + quantity +
+                "\tPricePerUnit: $" + ppu + "\tTotal Cost: $"
+                + getCost() + "\n";
+    }
+}
