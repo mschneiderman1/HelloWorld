97494c042589da8121fd288b6b202bc9534a8393
Matt Schneiderman <mschneiderman@student.mamkschools.org>
2018-12-21 10:00:18 -0500

    add

---
+++ src/Ch8Classes/GroceryProject/MattSchneidermanGroceryList.java
@@ -0,0 +1,33 @@
+package Ch8Classes.GroceryProject;
+
+import Ch8Classes.MattSchneidermanBankAccountV2;
+
+public class MattSchneidermanGroceryList{
+    public static final int MAX_ITEMS = 10;
+    //state fields
+    private MattSchneidermanGroceryItemOrder[] list;
+    private int numItems;
+
+    //constructor
+    public MattSchneidermanGroceryList(){
+        list = new MattSchneidermanGroceryItemOrder[MAX_ITEMS];
+        numItems = 0;
+    }
+    public void addItems(MattSchneidermanGroceryItemOrder item){
+        list[numItems] = item;
+        numItems++;
+    }
+    public double getTotalCost(){
+        double totalCost = 0;
+        for(int i = 0; i < numItems; i++){
+            totalCost += list[i].getCost();
+        }
+        return totalCost;
+    }
+    public String toString(){
+        String myList = "My Grocery List: \n";
+        for(int i = 0; i < numItems; i++){
+            myList +=  "Item : " + i + "\t" + list[i].toString();
+        }
+    }
+}
