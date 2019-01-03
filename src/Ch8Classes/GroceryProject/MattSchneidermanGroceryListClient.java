b0ba1bddb4d544a38338037c562b9c2c10e9a8bc
Matt Schneiderman <mschneiderman@student.mamkschools.org>
2019-01-03 13:36:24 -0500

    add

--- src/Ch8Classes/GroceryProject/MattSchneidermanGroceryListClient.java
+++ src/Ch8Classes/GroceryProject/MattSchneidermanGroceryListClient.java
@@ -3,11 +3,11 @@ package Ch8Classes.GroceryProject;
 public class MattSchneidermanGroceryListClient{
     public static void main(String[] args){
         MattSchneidermanGroceryList myList = new MattSchneidermanGroceryList();
-        myList.addItems(new MattSchneidermanGroceryItemOrder("Steak", 5, 10));
-        myList.addItems(new MattSchneidermanGroceryItemOrder("Potatoes", 10, 2));
-        myList.addItems(new MattSchneidermanGroceryItemOrder("Pineapple", 2,  5));
+        myList.addItems(new MattSchneidermanGroceryItemOrder("Steak         ", 5, 10));
+        myList.addItems(new MattSchneidermanGroceryItemOrder("Potatoes      ", 9, 2));
+        myList.addItems(new MattSchneidermanGroceryItemOrder("Pineapple     ", 2,  5));
         myList.addItems(new MattSchneidermanGroceryItemOrder("Chocolate Bars", 6, 2));
-        myList.addItems(new MattSchneidermanGroceryItemOrder("Iced Tea", 2, 5));
+        myList.addItems(new MattSchneidermanGroceryItemOrder("Iced Tea      ", 2, 5));
         System.out.println(myList);
     }
 }
