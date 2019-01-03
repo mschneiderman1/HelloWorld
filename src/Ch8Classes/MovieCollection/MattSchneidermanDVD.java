f26fc85f154fafb1c33a594b15dac8187785eace
Matt Schneiderman <mschneiderman@student.mamkschools.org>
2019-01-03 13:02:23 -0500

    add

--- src/Ch8Classes/MovieCollection/MattSchneidermanDVD.java
+++ src/Ch8Classes/MovieCollection/MattSchneidermanDVD.java
@@ -1,11 +1,20 @@
 package Ch8Classes.MovieCollection;
 
 public class MattSchneidermanDVD{
+    /**
+     * provides all the basic info of a movie
+     * title, director, year, cost and if its blueray
+     */
     private String title;
     private String director;
     private int year;
     private double cost;
     private boolean blueray;
+
+    /**
+     *initlializes the dvd and set all the parameters to
+     * their respective state field
+     */
 
     public MattSchneidermanDVD(String title, String director, int year, double cost, boolean blueray) {
         this.title = title;
@@ -15,9 +24,17 @@ public class MattSchneidermanDVD{
         this.blueray = blueray;
     }
 
+    /**
+     * returns the cost of the dvd
+     */
     public double getCost(){
         return cost;
     }
+
+    /**
+     * overrides the toString method and returns all the dvd information
+     * in the correct formation
+     */
     public String toString(){
         String ifBlueray = "";
         if(blueray == true){
