1faefc3f554d172f028eeb85d4062efe6b5808bd
Matt Schneiderman <mschneiderman@student.mamkschools.org>
2019-01-03 12:57:08 -0500

    add

--- src/Ch8Classes/MovieCollection/MattSchneidermanDVDCollection.java
+++ src/Ch8Classes/MovieCollection/MattSchneidermanDVDCollection.java
@@ -1,14 +1,32 @@
 package Ch8Classes.MovieCollection;
 
-public class MattSchneidermanDVDCollection {
+public class MattSchneidermanDVDCollection{
+        /**
+        * These are the state fields
+        * An array of the type MattSchneidermanDVD
+        * a int for th number of movies
+        * a double for the total value of the collection
+        */
     private MattSchneidermanDVD[] list;
     private int count;
     private double totalCost;
 
+    /**
+     * Constructor
+     *Initializes the array and gives it an initial length of 5
+     * set count to 0
+     */
     public MattSchneidermanDVDCollection(){
        list = new MattSchneidermanDVD[5];
        count = 0;
     }
+
+    /**
+     * increases the count by one
+     * checks to see if there is room in the array
+     * if not calls increaseSize()
+     * adds a dvd to the collection at index (count - 1)
+     */
     public void addDVD(MattSchneidermanDVD dvd){
         count++;
         if(count > list.length){
@@ -18,6 +36,12 @@ public class MattSchneidermanDVDCollection {
             list[count - 1] = dvd;
         }
     }
+
+    /**
+     * creates a copy of the array
+     * re-initializes MattSchneidermanDVD and makes it have a length of count
+     * it then transfers the contents from the copy back to the list array
+     */
     private void increaseSize(){
         MattSchneidermanDVD[] copy = list;
         list = new MattSchneidermanDVD[count];
@@ -25,6 +49,12 @@ public class MattSchneidermanDVDCollection {
             list[i] = copy[i];
         }
     }
+
+    /**
+     * finds the total cost of the collection
+     * then creates one large string of the dvd list using the array
+     * finally return all the info in the correction format
+     */
     public String toString(){
         totalCost = 0.0;
         for(int i = 0; i < count; i++){
