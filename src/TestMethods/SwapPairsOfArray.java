8c3101ce26817759b454f090adb724f3ab50c127
mschneidz <mss.tarheel2@gmil.com>
2019-01-15 21:26:38 -0500

    add

---
+++ src/TestMethods/SwapPairsOfArray.java
@@ -0,0 +1,21 @@
+package TestMethods;
+
+import java.util.Arrays;
+
+public class SwapPairsOfArray{
+    public static void main(String[] args){
+        String[] a = {"four", "score", "and", "seve","yea", "e" +
+                "" +
+                "" +
+                ""};
+        swapPairs(a);
+    }
+    public static void swapPairs(String[] a){
+        for(int i = 0; i < a.length - 1; i+=2){
+            String temp = a[i];
+            a[i] = a[i + 1];
+            a[i + 1] = temp;
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
