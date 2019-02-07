24f344801fd256b8a021d8c9f97102cfa8af1c68
mschneidz <mss.tarheel2@gmil.com>
2019-02-07 09:34:49 -0500

    add

---
+++ src/CH9Inheritance/EmployeeV1.java
@@ -0,0 +1,21 @@
+//A class to represent employees in general (20 page manual)
+package CH9Inheritance;
+
+public class EmployeeV1 {
+    public int getHours(){
+        return 40; // works 40 hours per week
+    }
+
+    public double getSalary(){
+        return 40000.0; //$40,000 a year
+    }
+
+    public int getVacationDays(){
+        return 10;  //2 weeks paid vacation
+    }
+
+    public String getVacationForm(){
+        return "yellow"; //use the yellow form for leave
+    }
+
+}
