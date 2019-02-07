71e8711383f46b8306b7d14cd986838c88112ce1
mschneidz <mss.tarheel2@gmil.com>
2019-02-07 09:46:28 -0500

    add

--- src/CH9Inheritance/LegalSecretaryV2.java
+++ src/CH9Inheritance/LegalSecretaryV2.java
@@ -2,18 +2,27 @@ package CH9Inheritance;
 
 public class LegalSecretaryV2 {
     public int getHours(){
+
         return 40; // works 40 hours per week
     }
 
     public double getSalary(){
+
         return 45000.0; //$45,000 a year
     }
 
     public int getVacationDays(){
+
         return 10;  //2 weeks paid vacation
     }
 
     public String getVacationForm(){
+
         return "yellow"; //use the yellow form for leave
+    }
+
+    public void takeDictation(String text){
+        System.out.println("Taking dictation of text: "
+            + text);
     }
 }
--- src/CH9Inheritance/SecretaryV2.java
+++ src/CH9Inheritance/SecretaryV2.java
@@ -2,18 +2,27 @@ package CH9Inheritance;
 
 public class SecretaryV2 {
     public int getHours(){
+
         return 40; // works 40 hours per week
     }
 
     public double getSalary(){
+
         return 40000.0; //$40,000 a year
     }
 
     public int getVacationDays(){
+
         return 10;  //2 weeks paid vacation
     }
 
     public String getVacationForm(){
+
         return "yellow"; //use the yellow form for leave
+    }
+
+    public void takeDictation(String text){
+        System.out.println("Taking dictation of text: "
+                + text);
     }
 }
