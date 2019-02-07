3b49ddb32418c3da68b90b353877369939622bff
mschneidz <mss.tarheel2@gmil.com>
2019-02-07 09:54:13 -0500

    add

--- src/CH9Inheritance/LegalSecretaryV2.java
+++ src/CH9Inheritance/LegalSecretaryV2.java
@@ -1,13 +1,8 @@
 package CH9Inheritance;
 
-public class LegalSecretaryV2 extends EmployeeV2{
+public class LegalSecretaryV2 extends SecretaryV2{
     public double getSalary() {
 
         return 45000.0; //$45,000 a year
-    }
-
-    public void takeDictation(String text){
-        System.out.println("Taking dictation of text: "
-            + text);
     }
 }
