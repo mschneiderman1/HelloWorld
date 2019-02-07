b2437a969da0120ec9973ce234326b2d31ae37cc
mschneidz <mss.tarheel2@gmil.com>
2019-02-07 09:41:52 -0500

    add

--- src/CH9Inheritance/SecretaryV2.java
+++ src/CH9Inheritance/SecretaryV2.java
@@ -1,26 +1,19 @@
 package CH9Inheritance;
 
-public class LawyerV2 {
+public class SecretaryV2 {
     public int getHours(){
         return 40; // works 40 hours per week
     }
 
     public double getSalary(){
-
         return 40000.0; //$40,000 a year
     }
 
     public int getVacationDays(){
-
-        return 15;  //3 weeks paid vacation
+        return 10;  //2 weeks paid vacation
     }
 
     public String getVacationForm(){
-
-        return "pink"; //use the pink form for leave
-    }
-
-    public void sue(){
-        System.out.println("I'll see you in court");
+        return "yellow"; //use the yellow form for leave
     }
 }
