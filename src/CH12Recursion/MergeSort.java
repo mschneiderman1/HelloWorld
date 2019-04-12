c2a977bf2fb3ec79bd5106ee6ca0a11b78da555a
mschneidz <mss.tarheel2@gmil.com>
2019-04-12 09:21:49 -0400

    Recursive Classes Chapter 12 (Merge Sort)

--- src/CH12Recursion/MergeSort.java
+++ src/CH12Recursion/MergeSort.java
@@ -4,40 +4,39 @@ import java.util.Arrays;
 
 public class MergeSort{
     public static void main(String[] args){
-        int[] array = {1, 2, 3, 4, 5 , 6};
-        int[] array1 = {1, 2, 3};
-        int[] array2 = {4, 5, 6};
-        System.out.println(Arrays.toString(merge(array, array1, array2)));
+        int[] array = {5, 10, 12, 2, 9, 6, 13};
+        System.out.println(Arrays.toString(array));
+        mergeSort(array);
+        System.out.println(Arrays.toString(array));
 
     }
     public static void mergeSort(int[] array){
-        //split array into two halves
-        int[] left = Arrays.copyOfRange(array, 0,array.length / 2);
-        int[] right = Arrays.copyOfRange(array, array.length / 2, array.length);
+        if(array.length >= 2) {
+            //split array into two halves
+            int[] left = Arrays.copyOfRange(array, 0, array.length / 2);
+            int[] right = Arrays.copyOfRange(array, array.length / 2, array.length);
 
-        //sort the two halves
-        System.out.println(Arrays.toString(left));
-        System.out.println(Arrays.toString(right));
-        mergeSort(left);
-        mergeSort(right);
+            //sort the two halves
+            mergeSort(left);
+            mergeSort(right);
 
-        //merge the sorted halves into a sorted whole
-        merge(array, left, right);
+            //merge the sorted halves into a sorted whole
+            merge(array, left, right);
+        }
     }
-    public static int[] merge(int[] array, int[] left, int[] right){
-        int[] sorted = {array.length};
+    public static void merge(int[] array, int[] left, int[] right){
         int i1 = 0;
         int i2 = 0;
-        for(int i = 0; i < array.length ; i++){
-             if((i2 >= right.length || i1 < left.length) && left[i1] <= right[i2]){
-                 sorted[i] = left[i1];
+        for(int i = 0; i < array.length; i++){
+             if(i2 >= right.length ||
+                     (i1 < left.length && left[i1] < right[i2])){
+                 array[i] = left[i1];
                  i1++;
              }else{
-                 sorted[i] = right[i2];
+                 array[i] = right[i2];
                  i2++;
 
              }
         }
-        return sorted;
     }
 }
