96b55cc85e73e6f7f0fc5dbf6faaf85f9a0ccf27
mschneidz <mss.tarheel2@gmil.com>
2019-04-25 09:27:55 -0400

    Recursive Classes Chapter 12 (Merge Sort)

---
+++ src/CH12Recursion/MattSchneidermanInsertionSort.java
@@ -0,0 +1,9 @@
+package CH12Recursion;
+
+public class MattSchneidermanInsertionSort{
+    public static void main(String[] args){
+    }
+    public static void insertionSort(int[] array){
+
+    }
+}
--- src/CH12Recursion/MattSchneidermanSelectionSort.java
+++ src/CH12Recursion/MattSchneidermanSelectionSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MattSchneidermanSelectionSort {
     public static void main(String[] args){
-        int[] array = {9,3,6,2,4,1,9};
+        int[] array = {9,3,6,2,4,1,9,12,345,676,7,3,6,4};
         System.out.println(Arrays.toString(selectionSort(array)));
 
     }
